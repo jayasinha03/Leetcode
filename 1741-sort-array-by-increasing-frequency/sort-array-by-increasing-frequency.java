2b711082515bde54c2190de06806ec04b29795d5
Jaya Sinha <85779387+jayasinha03@users.noreply.github.com>
2024-07-23 23:27:37 +0530

    Time: 8 ms (46.12%) | Memory: 44.7 MB (8.00%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
@@ -1,25 +1,30 @@
 class Solution {
     public int[] frequencySort(int[] nums) {
         Map<Integer, Integer> frequencyMap = new HashMap<>();
-        
-        // Calculate the frequency of each element
+
         for (int num : nums) {
             frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
         }
+
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            list.add(num);
+        }
+
+        Collections.sort(list, (a, b) -> {
+            int freqA = frequencyMap.get(a);
+            int freqB = frequencyMap.get(b);
+            if (freqA == freqB) {
+                return b - a;
+            } else {
+                return freqA - freqB; 
+            }
+        });
         
-        // Sort the array based on the frequency and then by value
-        return Arrays.stream(nums)
-                .boxed()
-                .sorted((a, b) -> {
-                    int freqA = frequencyMap.get(a);
-                    int freqB = frequencyMap.get(b);
-                    if (freqA == freqB) {
-                        return b - a; 
-                    } else {
-                        return freqA - freqB;
-                    }
-                })
-                .mapToInt(i -> i)
-                .toArray();
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = list.get(i);
+        }
+        
+        return nums;
     }
 }
