9480c91f8e7712905ac7d5f68051c0d3de92db5c
Jaya Sinha <85779387+jayasinha03@users.noreply.github.com>
2024-07-23 23:03:53 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (93.24%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
@@ -13,14 +13,4 @@ class Solution {
         // Delete the next node by updating the current node's next pointer
         node.next = node.next.next;
     }
-
-    // Helper function to print the linked list
-    public void printLinkedList(ListNode head) {
-        ListNode current = head;
-        while (current != null) {
-            System.out.print(current.val + " -> ");
-            current = current.next;
-        }
-        System.out.println("null"); 
-    }
 }
